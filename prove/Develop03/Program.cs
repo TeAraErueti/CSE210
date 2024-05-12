15e3e0c96f9279f424eed2c54c9bfe72019cab1c
TeAraErueti <eru24002@byui.edu>
2024-05-12 17:32:49 +1200

    program scripture done

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Console.WriteLine("--------- Welcome to the Scripture Memorizer --------");
+
+        Reference theReference = new(book:"", chapter:0, verse_s:0);
+
+        Console.Write("Please enter the scripture in the format - (Book, Chapter, Verse/s) seperated by a comma , ): "); 
+        string[] listRef = Console.ReadLine().Split(",");
+
+        if (listRef.Count() == 4)
+        {
+            theReference = new Reference(book:listRef[0], chapter:int.Parse(listRef[1]), verse_s:int.Parse(listRef[2]), verse_e:int.Parse(listRef[3]));
+
+        }
+        
+        if (listRef.Count() == 3)
+        {
+            theReference = new Reference(book:listRef[0], chapter:int.Parse(listRef[1]), verse_s:int.Parse(listRef[2])); 
+        }
+        
+        Console.WriteLine("Please enter the scripture verse:\n");
+        theReference.setVerse();
+        Scripture scripture = new(theReference); 
+        
+
+        string isQuit = "";
+        while (isQuit != "quit")
+        {
+              
+            if(scripture.IsCompletelyHidden())
+            {
+                break;
+            }
+
+            scripture.DisplayText();
+            Console.Clear();
+            scripture.HideRandomWords(5);
+            Console.Clear();
+            scripture.DisplayText();
+
+            Console.WriteLine("Please press Enter to continue or type 'quit' to leave the program: ");
+            isQuit = Console.ReadLine().ToLower();
+        }
+            
     }
 }
