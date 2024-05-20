abc24b08be4afb5d89ef92e73450e243d9db4c28
TeAraErueti <eru24002@byui.edu>
2024-05-20 18:28:03 +1200

    shapes program complete

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square("Blue", 4);
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle("Green", 7, 5);
+        shapes.Add(shape2);
+
+        Circle shape3 = new Circle("Violet", 9);
+        shapes.Add(shape3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+
     }
 }
