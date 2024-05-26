bff99f72677daaf00d582e752ea8a91e8a9ed8c3
TeAraErueti <eru24002@byui.edu>
2024-05-26 17:10:55 +1200

    eternal quest program complete

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,15 @@
+// exceeding requirement: setup a folder called Goalfolder for goals to be saved for easy use in future.
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        Console.WriteLine("\n***** Welcome to the Eternal Quest Program *****\n");
+
+        GoalManager goalManager = new();
+
+        goalManager.Start();
+
     }
 }
